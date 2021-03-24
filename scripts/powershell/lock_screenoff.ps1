0a43af1813cbcb09906567eab217a64ba9e468e2
Alexandro Sanchez <alexandro@phi.nz>
2021-03-24 16:35:16 +0100

    Create lock_screenoff.ps1

---
+++ scripts/powershell/lock_screenoff.ps1
@@ -0,0 +1,5 @@
+# Lock machine
+rundll32.exe user32.dll, LockWorkStation;
+
+# Turn off screen
+(Add-Type '[DllImport("user32.dll")]public static extern int SendMessage(int hWnd, int hMsg, int wParam, int lParam);' -Name a -Pas)::SendMessage(-1,0x0112,0xF170,2)
